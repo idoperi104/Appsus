c25049c4b53ac952d892d7d20ee7608594070c6a
idoperi104 <idoperi104@gmail.com>
2023-03-01 10:06:50 +0200

    good luck

---
+++ cmps/AppHeader.js
@@ -0,0 +1,27 @@
+export default {
+	template: `
+        <header class="app-header">
+            <h1>AppSus</h1>
+            <nav>
+                <RouterLink v-for="(route, idx) in routs" :to="route.path"
+                :title="route.name" :key="idx">{{route.name}}</RouterLink>
+            </nav>
+        </header>
+    `,
+    data() {
+        return {
+            routs: [
+                { path: '/', name: 'Home |' },
+                { path: '/about', name: 'About |' },
+                { path: '/mail', name: 'Mail |' },
+                { path: '/keep', name: 'Keep |' }
+            ]
+        }
+    },
+    methods: {
+        setRoute(route) {
+            this.$emit('set-route', route)
+        }
+    }
+}
+
---
+++ routes.js
@@ -0,0 +1,30 @@
+import HomePage from './views/HomePage.js'
+import AboutUs from './views/AboutUs.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
+import MailIndex from './apps/mail/pages/MailIndex.js'
+
+const { createRouter, createWebHashHistory } = VueRouter
+
+const routerOptions = {
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: '/',
+			component: HomePage,
+		},
+		{
+			path: '/about',
+			component: AboutUs,
+		},
+		{
+			path: '/mail',
+			component: MailIndex,
+		},
+		{
+			path: '/keep',
+			component: NoteIndex,
+		},
+	],
+}
+
+export const router = createRouter(routerOptions)
