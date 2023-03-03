02e121a15c30f559fed1a4d5ee2aca2d6655cd29
idoperi104 <idoperi104@gmail.com>
2023-03-03 11:25:00 +0200

    note- layout styling

--- apps/keep/cmps/NoteAdd.js
+++ apps/keep/cmps/NoteAdd.js
@@ -4,20 +4,23 @@ import { showUserMsg, showSuccessMsg, showErrorMsg } from '../../../services/eve
 export default {
     template: `
         <section class="note-add">
-            <form @click.stop="isShowAll = true" @submit.prevent="save" autocomplete="on">
+            <form @click.stop="atNewAdd" @submit.prevent="save" autocomplete="on" :class="getFormClass">
 
-                <input v-if="isShowAll" name="title" type="text" v-model="note.info.title" placeholder="title">
+                <input v-if="isShowAll" name="title" type="text" v-model="note.info.title" placeholder="title:">
                 
                 <input ref="focusInput" v-if="note.type === 'NoteTxt'"
                      name="txt" type="text" v-model="note.info.txt" 
                      autocomplete="off" placeholder="take a note...">
                 
-                <input v-if="note.type === 'NoteImg' || note.type === 'NoteVideo'" name="url" type="text" v-model="note.info.url" placeholder="enter url...">
+
+                <img v-if="note.type === 'NoteImg' && note.info.url" :src="note.info.url"/>
+                <input v-if="note.type === 'NoteImg' || note.type === 'NoteVideo'" name="url" type="text"
+                    autocomplete="off" v-model="note.info.url" placeholder="enter url...">
                 
                 <label v-if="isShowAll" for="bg-color">bg-color:</label>
                 <input v-if="isShowAll" name="bg-color" type="color" v-model="note.style.backgroundColor" placeholder="write note">
 
-                <button class="btn-save">Save</button>
+                <button v-if="isShowAll" class="btn-save">Save</button>
 
                         
                 <div class="note-edit-type">
@@ -44,7 +47,9 @@ export default {
     created() {
     },
     computed: {
-        
+        getFormClass(){
+            return this.isShowAll? 'show-all' : ''
+        }
     },
     watch: {
     },
@@ -68,10 +73,15 @@ export default {
         },
         setIsShowFalse(){
             this.isShowAll = false
+        },
+        atNewAdd(){
+            this.isShowAll = true
+            this.note.isPinned = true
         }
     },
     mounted() {
         window.addEventListener('click', this.setIsShowFalse)
+        // this.note.isPinned = true
     },
     unmounted() {
         window.removeEventListener('click', this.setIsShowFalse)
